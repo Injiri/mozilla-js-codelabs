2c04dae534b3116c8be24e0f7e02bb2fea344f4a
samuelowino <samuelowino43@yahoo.com>
2020-12-07 17:44:44 +0300

    Promises and Sync Await

--- intoduction/section-03/scripts/index.js
+++ intoduction/section-03/scripts/index.js
@@ -3,3 +3,25 @@
 //=====================================================================================
 console.log("=================================================================")
 console.log("Ayncronouse Javascript | Promise | async-wait | EventLoop |");
+
+async function caclucateYears() {
+    console.log("Calculating years...");
+    let sum = 0;
+    for (i = 0; i < 9000000; i++) {
+        sum += 900000 * 900000 * 900000 * 9000000;
+    }
+
+    return sum;
+}
+
+
+function logYears(years) {
+    console.log(years);
+}
+
+
+async function resolveYears() {
+    await caclucateYears().then(logYears);
+}
+
+resolveYears();
